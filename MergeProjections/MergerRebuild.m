39beaa0f052ad22016d34bdfd0b6d7ae32f486ad
(no author) <(no author)@b4bb190c-55cf-4028-80a8-437e0b259801>
2010-06-08 15:55:54 +0000

    - MergerRebuild.m is proceeding nice. Contemplating on using SIFT for finding cutline... - SIFT_finder.m is a script that loads images, calculates the features with SIFT and aims to extract the cutline. Further work is needed, though...
    git-svn-id: http://code.ana.unibe.ch/svn/wfs-sim@83 b4bb190c-55cf-4028-80a8-437e0b259801

--- MergeProjections/MergerRebuild.m
+++ MergeProjections/MergerRebuild.m
@@ -5,21 +5,26 @@
 % Initial Version: 3.6.2010
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc;clear all;close all;
+WasDir = pwd;
 
 %% Setup
-Drive = 'R';
-BeamTime = '2010b';
-ReadLinesOfLogFile = 33;
+Drive = 'R'; BeamTime = '2010b'; % Setup for Windows
+ReadLinesOfLogFile = 33; % Lines to read for the Logfile, so we don't read in everything
+Cutline = 111;
 
 %% Ask the User what SubScans we should merge
-h=helpdlg('Select the logfile of the FIRST SubScan I should merge',...
-    'Instructions');
-uiwait(h);
-pause(0.01);
-StartPath = [ Drive ':' filesep 'SLS' filesep BeamTime filesep 'log' ];
+% h=helpdlg('Select the logfile of the FIRST SubScan I should merge',...
+%     'Instructions');
+% uiwait(h);
+% pause(0.01);
+if isunix==0
+    StartPath = [ Drive ':' filesep 'SLS' filesep BeamTime filesep 'log' ];
+else
+    StartPath = [ filesep 'sls' filesep 'X02DA' ];
+end
 disp(['Opening ' StartPath ' to look for Logfiles'])
-cd(StartPath)
-[ LogFile, LogFilePath] = uigetfile('*.log','Select a LogFile','LogFile.log');
+[ LogFile, LogFilePath] = uigetfile({'*.log','LogFiles (*.log)'},'Pick the FIRST LogFile',...
+          [ StartPath filesep 'LogFile.log' ])
 [ SubScan Starting Ending] = regexp(LogFile, '_s1', 'match', 'start', 'end');
 
 %% See how many LogFiles we're having...
@@ -40,13 +45,21 @@ for i=1:7
         break
     end
 end
-disp('---')
 disp([ 'I have found ' num2str(AmountOfSubScans) ' logfiles which seem to belong together in ' LogFilePath ])
+if AmountOfSubScans == 0
+    disp('Quitting')
+    break
+end
 Data(AmountOfSubScans).Dummy = NaN; % preallocate Structure for Speed-purposes
 for i=1:AmountOfSubScans
+    Data(i).Cutline = Cutline; % Initialize Cutlines empty (or with value set on Line ~13, so we can skip cutline-detection
+end
+for i=1:AmountOfSubScans
     disp(['-' LogFile(1:Starting-1) '_s' num2str(i) LogFile(Ending+1:end) ]);
-    Data(i).LogFileLocation = [ LogFilePath LogFile(1:Starting-1) '_s' num2str(i) LogFile(Ending+1:end) ];
-	Data(i).LogFileName = [ LogFile(1:Starting-1) '_s' num2str(i) LogFile(Ending+1:end) ];
+    Data(i).LogFileLocation = [ LogFilePath LogFile(1:Starting-1) '_s' ...
+        num2str(i) LogFile(Ending+1:end) ];
+	Data(i).LogFileName = [ LogFile(1:Starting-1) '_s' num2str(i) ...
+        LogFile(Ending+1:end) ];
     Data(i).SubScanName = [ LogFile(1:Starting-1) '_s' num2str(i) '_' ];
 end
 disp('---')
@@ -61,16 +74,16 @@ for i=1:AmountOfSubScans
     % Above line splits LogFile-lines at ':' so we can extract the values
     % get Values and strip leading and trailing spaces
     Data(i).UserID = strtrim(TMP{1}{2});
-    Data(i).RingCurrent = strtrim(TMP{4}{2});
-    Data(i).BeamEnergy = strtrim(TMP{5}{2});
+    Data(i).RingCurrent = str2double(strtrim(TMP{4}{2}));
+    Data(i).BeamEnergy = str2double(strtrim(TMP{5}{2}));
     Data(i).Mono = strtrim(TMP{6}{2});
-    Data(i).Objective = strtrim(TMP{8}{2});
+    Data(i).Objective = str2double(strtrim(TMP{8}{2}));
     Data(i).Scintillator = strtrim(TMP{9}{2});
-    Data(i).ExposureTime = strtrim(TMP{10}{2});
+    Data(i).ExposureTime = str2double(strtrim(TMP{10}{2}));
     Data(i).SampleFolder = strtrim(TMP{12}{2});
-    Data(i).Projections = strtrim(TMP{14}{2});
-    Data(i).Darks = strtrim(TMP{15}{2});
-    Data(i).Flats = strtrim(TMP{16}{2});
+    Data(i).Projections = str2double(strtrim(TMP{14}{2}));
+    Data(i).NumDarks = str2double(strtrim(TMP{15}{2}));
+    Data(i).NumFlats = str2double(strtrim(TMP{16}{2}));
     disp(['Extracting Data from ' Data(i).LogFileName ]);
 end
 disp('---');
@@ -80,23 +93,24 @@ disp('---');
 if AmountOfSubScans == 3
     disp('According to the Logfiles we recorded:');      
     for i=1:AmountOfSubScans
-        disp([ num2str(Data(i).Projections) ' Projections for SubScan ' Data(i).SubScanName]);      
+        disp([ num2str(Data(i).Projections) ' Projections for SubScan ' ...
+            Data(i).SubScanName]);      
     end
 else
     disp('Merging for other amount than 3 SubScans not implemented yet...');
     disp('Quitting');
     break
 end
-disp('---')
 % Projections
-if Data(1).Projections == Data(2).Projections
+Interpolation = Data(1).Projections / Data(2).Projections;
+if Interpolation == 1
     disp('SubScans _s1 and _s2 have an equal amount of Projections.')
     disp('No Interpolation necessary while merging');
-    doInterpolate = 0;
 else
     disp('SubScans _s1 and _s2 have a different amount of Projections.')
     disp('Interpolating during merging');
-    doInterpolate = 1;
+    disp([ 'Interpolation-Factor from SubScan _s1 to _s2 is ' ...
+        num2str(Interpolation) ])
 end
 disp('---');
 
@@ -109,14 +123,98 @@ if isunix == 1
 else
     disp('probably @Unibe, constructing new Location')
     for i=1:AmountOfSubScans        
-        Data(i).SampleFolder = [ Drive ':' filesep 'SLS' filesep BeamTime filesep Data(i).SubScanName ];     
+        Data(i).SampleFolder = [ Drive ':' filesep 'SLS' filesep BeamTime ...
+            filesep Data(i).SubScanName ];     
+    end
+end
+disp('---');
+
+%%Load Darks and Flats
+disp('Loading Darks and Flats');
+for i=1:AmountOfSubScans
+    for k=1:Data(i).NumDarks
+        Data(i).Dark(:,:,k) = imread([Data(i).SampleFolder filesep 'tif' ...
+            filesep Data(i).SubScanName sprintf('%04d',k) '.tif' ]);
+        Data(i).Dark = double(Data(i).Dark);
+    end
+    for k=1:Data(i).NumFlats
+        Data(i).Flat(:,:,k) = imread([Data(i).SampleFolder filesep 'tif' ...
+            filesep Data(i).SubScanName sprintf('%04d',k+Data(i).NumDarks) '.tif' ]);
+        Data(i).Flat = double(Data(i).Flat);
     end
 end
 
-%% Showing Files
-figure
+% Average Darks & Flats
 for i=1:AmountOfSubScans
-    Data(i).Projection = imread([Data(i).SampleFolder filesep 'tif' filesep Data(i).SubScanName '0128.tif' ]);
-    subplot(1,3,i)
-    imshow(Data(i).Projection,[])
+    Data(i).AverageDark = mean(Data(i).Dark,3);
+    Data(i).AverageFlat = mean(Data(i).Flat,3);
+end
+
+% Show Darks * Flats
+figure
+    for i=1:AmountOfSubScans
+        subplot(2,3,i)
+            imshow(Data(i).AverageDark,[])
+            title('Dark')
+        subplot(2,3,i+3)
+            imshow(Data(i).AverageFlat,[])
+            title('Flat')
+    end
+disp('---');
+
+%% Loop over Projections, extract cutline, show files (if necessary), merge
+%% projections and write out to disk.
+disp('Loading Projections');
+for k=500%(Data(i).NumDarks + Data(i).NumFlats +1):750:(Data(i).NumDarks + Data(i).NumFlats + Data(i).Projections + Data(i).NumFlats)
+    disp(['Working on Projections ' num2str(k) ]);
+    % Load Projections
+    for i=1:AmountOfSubScans
+        Data(i).Projection = imread([Data(i).SampleFolder filesep 'tif' ...
+                filesep Data(i).SubScanName num2str(sprintf('%04d',k)) '.tif' ]);
+        Data(i).Projection = double(Data(i).Projection);
+        Data(i).CorrectedProjection = log(Data(i).AverageFlat - Data(i).AverageDark) - log(Data(i).Projection - Data(i).AverageDark);
+    end
+    
+    %% Cutline Extraction
+    for i=1:AmountOfSubScans-1
+        if isempty(Data(i).Cutline)
+            disp('Extracting Cutline, this will take some time');
+            disp(['Extracting Cutline between _s' num2str(i) ' and _s' num2str(i+1) ]);
+            Data(i).Cutline = function_cutline(Data(i).CorrectedProjection,Data(i+1).CorrectedProjection);
+        end
+        disp(['The Cutline between _s' num2str(i) ' and _s' num2str(i+1) ' was found to be: ' num2str(Data(i).Cutline) ]);
+    end
+    disp('---');
+    
+    %% Merge Projections
+    Data(1).CroppedProjection = Data(1).CorrectedProjection(:,1:end-Data(1).Cutline);
+    Data(2).CroppedProjection = Data(2).CorrectedProjection;
+    Data(3).CroppedProjection = Data(3).CorrectedProjection(:,Data(2).Cutline+1:end);
+    MergedProjection = [ Data(1).CroppedProjection Data(2).CroppedProjection Data(3).CroppedProjection ];
+    BlaBla = [];
+    if isempty(BlaBla)
+        disp(['The original Projections have a size of ' num2str(size(Data(1).Projection,1)) ...
+            'x' num2str(size(Data(1).Projection,2)) ' pixels.' ]);
+        disp(['The cutlines were found to be ' num2str(Data(1).Cutline) ...
+            ' (s1:s2) and ' num2str(Data(2).Cutline) ' (s2:s3)']);
+        disp(['The merged Projection should thus have ' num2str(size(Data(1).Projection,1)) ...
+            'x' num2str((AmountOfSubScans*size(Data(1).Projection,1))-Data(1).Cutline-Data(2).Cutline) ...
+            ' pixels ((' num2str(AmountOfSubScans) '*' num2str(size(Data(1).Projection,1)) ...
+            ')-' num2str(Data(1).Cutline) '-' num2str(Data(2).Cutline) ')' ]);
+        disp(['In realty it has ' num2str(size(MergedProjection,1)) 'x' ...
+            num2str(size(MergedProjection,2)) ' pixels' ]);
+        BlaBla = 1;
+    end
+    
+    %% Showing Files
+    figure('Name',[ 'Projection ' num2str(k) ])
+    for i=1:AmountOfSubScans
+        subplot(2,3,i)
+            imshow(Data(i).Projection,[])
+            title(['SubScan _s' num2str(i)],'Interpreter','None')            
+    end
+        subplot(2,3,4:6)
+        imshow(MergedProjection,[])
+        title(['Merged and corrected Projections ' num2str(k)])
+    pause(0.001)
 end
---
+++ MergeProjections/SIFT_finder.m
@@ -0,0 +1,66 @@
+%% SIFT-Test
+% Testing SIFT-algorithm from http://www.vlfeat.org/
+clc;clear all;close all
+
+%% Load Files
+for i=100:250:3500
+    disp(['Working on projection ' num2str(i) ])
+    disp('Reading Files')
+    Ia = double(imread(['R:\SLS\2010b\R108C04Aa_B1_s1_\tif\R108C04Aa_B1_s1_' sprintf('%04d',i) '.tif']));
+    Ib = double(imread(['R:\SLS\2010b\R108C04Aa_B1_s2_\tif\R108C04Aa_B1_s2_' sprintf('%04d',i) '.tif']));
+    DarkA = double(imread('R:\SLS\2010b\R108C04Aa_B1_s1_\tif\R108C04Aa_B1_s1_0004.tif'));
+    DarkB = double(imread('R:\SLS\2010b\R108C04Aa_B1_s2_\tif\R108C04Aa_B1_s2_0004.tif'));
+    FlatA = double(imread('R:\SLS\2010b\R108C04Aa_B1_s1_\tif\R108C04Aa_B1_s1_0040.tif'));
+    FlatB = double(imread('R:\SLS\2010b\R108C04Aa_B1_s2_\tif\R108C04Aa_B1_s2_0040.tif'));
+    Ia = mat2gray(log(FlatA-DarkA)-log(Ia-DarkA));
+    Ib = mat2gray(log(FlatB-DarkB)-log(Ib-DarkB));
+
+    %% Prepare Images for SIFT
+    Ia = single(Ia);
+    Ib = single(Ib);
+
+    %Crop Images
+    CropTo = 444;
+    Ia = Ia(:,end-CropTo+1:end);
+    Ib = Ib(:,1:CropTo);
+
+    %% Calculate SIFT
+    disp('Calculating SIFT')
+    [fa, da] = vl_sift(Ia);
+    [fb, db] = vl_sift(Ib);
+    [matches scores] = vl_ubcmatch(da,db,3);
+    [drop, perm] = sort(scores, 'descend');
+    matches = matches(:,perm);
+    scores  = scores(perm);
+
+    %% Prepare Figure
+    shift = 25;
+
+    %Calculate stuff for figure
+    xa = fa(1,matches(1,:));
+    cutlinea=round(mean(xa));
+    xb = fb(1,matches(2,:));
+    cutlineb=round(mean(xb));
+    if isnan(cutlinea) | isnan(cutlineb)
+        disp(['!!!! no cutline found for projection ' num2str(i) '!!!!'])
+        cutlinea=1;
+        cutlineb=cutlinea;
+    end
+    xb = xb + size(Ia,2) + shift;
+    ya = fa(2,matches(1,:));
+    yb = fb(2,matches(2,:));
+    fb(1,:) = fb(1,:) + size(Ia,2) + shift;
+    MergedProjection=([ Ia(:,1:cutlinea) Ib(:,cutlineb:end) ]);
+
+    figure(i)
+        subplot(121)
+            imshow([ Ia zeros(size(Ia,1),shift) Ib ],[]);
+            hold on;
+            h = line([xa; xb], [ya; yb]);
+            set(h,'linewidth', 2, 'color', 'b');
+            vl_plotframe(fa(:,matches(1,:)));
+            vl_plotframe(fb(:,matches(2,:)));
+        subplot(122)
+            imshow(MergedProjection,[])
+    disp('---')
+end
